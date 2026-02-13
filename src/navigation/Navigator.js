582d7eb6d2b6547cca73dfa4166bebfeb42be8e8
fatihsenguun <fatihsenguun@gmail.com>
2026-02-13 09:35:04 +0300

    navigator

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Animated, Dimensions, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image, Animated, Dimensions, TouchableOpacity, Platform } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 
 import Home from '../pages/Home'
@@ -12,7 +12,7 @@ import Favourites from '../pages/Favourites'
 import ProductPage from '../pages/ProductPage'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import SearchResults from '../pages/SearchResults'
 
 
@@ -20,148 +20,139 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 
+// ===== TABS BILEŞENINI NAVIGATOR DIŞINA TAŞI =====
+const Tabs = ({ slideAnim, widthAnim, opacityAnim }) => {
+    return (
+        <Tab.Navigator screenOptions={{
+            tabBarActiveTintColor: "#520000",
+            tabBarLabelStyle: {
+                fontSize: 14,
+                fontFamily: Platform.OS === 'ios' ? 'Times New Roman' : 'serif',
+                fontWeight: '400',
+            },
+
+            tabBarStyle: {
+                paddingHorizontal: 15
+            },
+            headerLeft: () => (
+                <Animated.Image
+                    source={require('../images/logo.png')}
+                    style={{
+                        width: widthAnim,
+                        height: 100,
+                        opacity: opacityAnim,
+                        transform: slideAnim.getTranslateTransform(),
+                    }}
+                />
+            ),
+
+        }}>
+            <Tab.Screen
+                name="HOME"
+                component={HomeStack}
+                options={{
+                    headerTitle: "",
+                    tabBarIcon: () => {
+                        <Image style={styles.basket} source={require('../images/shopping-bag.png')} />
+                    },
+
+                }}
+            />
+            <Tab.Screen name="SEARCH" component={SearchStack}
+                options={{
+                    headerTitle: "",
+                    tabBarIcon: () => null,
+
+                }} />
+            <Tab.Screen name={" "} component={Favourites}
+                options={{
+                    headerTitle: "",
+
+                    tabBarIcon: ({ focused, color }) => (
+                        !focused ?
+                            (<Image style={[styles.heart, { tintColor: color }]} source={require('../images/heart23.png')} />) :
+                            (<Image style={[styles.heart, { tintColor: color }]} source={require('../images/heartfill.png')} />)
+                    ),
+                    tabBarIconStyle: {
+                        marginTop: 20,
+                    },
+
+                    tabBarActiveTintColor: "#520000"
+                }}
+            />
+            <Tab.Screen name="CART" component={Cart}
+                options={{
+                    headerTitle: "",
+                    tabBarIcon: () => null,
+
+                }} />
+            <Tab.Screen name="PROFILE" component={Profile}
+                options={{
+                    headerTitle: "",
+                    tabBarIcon: () => null,
+
+                }} />
+
+        </Tab.Navigator>
+    )
+}
 
 const Navigator = () => {
 
+    const { width, height } = Dimensions.get('window');
 
+    const slideAnim = useRef(new Animated.ValueXY({ x: (width / 2) - 200, y: (height / 2) - 50 })).current;
+    const widthAnim = useRef(new Animated.Value(400)).current;
+    const opacityAnim = useRef(new Animated.Value(0)).current;
 
-    const Tabs = () => {
+    useEffect(() => {
+        Animated.timing(opacityAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: false,
 
-        const { width, height } = Dimensions.get('window');
+        }).start()
+    }, [])
 
-        const slideAnim = useRef(new Animated.ValueXY({ x: (width / 2) - 200, y: (height / 2) - 50 })).current;
-        const widthAnim = useRef(new Animated.Value(400)).current;
-        const opacityAnim = useRef(new Animated.Value(0)).current;
+    useEffect(() => {
+        const timeout = setTimeout(() => {
 
-        useEffect(() => {
-            Animated.timing(opacityAnim, {
-                toValue: 1,
-                duration: 500,
-                useNativeDriver: false,
+            Animated.parallel([
 
-            }).start()
-        }, [])
-        useEffect(() => {
+                Animated.timing(slideAnim, {
+                    toValue: { x: 0, y: 0 },
+                    duration: 1000,
+                    useNativeDriver: false,
+                }),
+                Animated.timing(widthAnim, {
+                    toValue: 140,
+                    duration: 1000,
+                    useNativeDriver: false,
+                })
 
-            const timeout = setTimeout(() => {
+            ]).start();
 
-                Animated.parallel([
+        }, 1300)
 
-                    Animated.timing(slideAnim, {
-                        toValue: { x: 0, y: 0 },
-                        duration: 1000,
-                        useNativeDriver: false,
-                    }),
-                    Animated.timing(widthAnim, {
-                        toValue: 140,
-                        duration: 1000,
-                        useNativeDriver: false,
-                    })
+        return () => clearTimeout(timeout);
+    }, [])
 
-                ]).start();
-
-            }, 1300)
-
-        }, [])
-
-        return (
-
-            <Tab.Navigator screenOptions={{
-                tabBarActiveTintColor: "#520000",
-                tabBarLabelStyle: {
-                    fontSize: 14,
-                    fontFamily: Platform.OS === 'ios' ? 'Times New Roman' : 'serif',
-                    fontWeight: '400',
-                },
-
-                tabBarStyle: {
-                    paddingHorizontal: 15
-                },
-                headerLeft: () => (
-                    <Animated.Image
-                        source={require('../images/logo.png')}
-                        style={{
-                            width: widthAnim,
-                            height: 100,
-                            opacity: opacityAnim,
-                            transform: slideAnim.getTranslateTransform(),
-                        }}
-                    />
-                ),
-
-
-            }}>
-                <Tab.Screen
-                    name="HOME"
-                    component={HomeStack}
-                    options={{
-                        headerTitle: "",
-                        tabBarIcon: () => {
-                            <Image style={styles.basket} source={require('../images/shopping-bag.png')} />
-                        },
-
-                    }}
-                />
-                <Tab.Screen name="SEARCH" component={SearchStack}
-                    options={{
-                        headerTitle: "",
-                        tabBarIcon: () => null,
-
-
-                    }} />
-                <Tab.Screen name={" "} component={Favourites}
-                    options={{
-                        headerTitle: "",
-
-                        tabBarIcon: ({ focused, color }) => (
-                            !focused ?
-                                (<Image style={[styles.heart, { tintColor: color }]} source={require('../images/heart23.png')} />) :
-                                (<Image style={[styles.heart, { tintColor: color }]} source={require('../images/heartfill.png')} />)
-                        ),
-                        tabBarIconStyle: {
-                            marginTop: 20,
-                        },
-
-                        tabBarActiveTintColor: "#520000"
-                    }}
-                />
-                <Tab.Screen name="CART" component={Cart}
-                    options={{
-                        headerTitle: "",
-                        tabBarIcon: () => null,
-
-                    }} />
-                <Tab.Screen name="PROFILE" component={Profile}
-                    options={{
-                        headerTitle: "",
-                        tabBarIcon: () => null,
-
-                    }} />
-
-
-            </Tab.Navigator>
-
-
-        )
-    }
     return (
         <NavigationContainer>
 
             <Stack.Navigator>
 
-
                 <Stack.Screen
                     name="MainTabs"
-                    component={Tabs}
+                    component={() => <Tabs slideAnim={slideAnim} widthAnim={widthAnim} opacityAnim={opacityAnim} />}
                     options={{ headerShown: false }}
                 />
-
 
                 <Stack.Screen
                     name="SearchResults"
                     component={SearchResults}
 
-                    options={{
+                    options={({ navigation }) => ({
                         headerShown: true,
                         title: false,
                         headerBackTitleVisible: false,
@@ -170,16 +161,18 @@ const Navigator = () => {
 
                         headerTintColor: "#520000",
                         headerRight: () => (
-                            <View></View>
+                            <TouchableOpacity onPress={() => navigation.navigate('MainTabs', { screen: 'CART' })}>
+                                <Image style={styles.basket} source={require('../images/shopping-bag.png')} />
+                            </TouchableOpacity>
                         ),
-                    }}
+                    })}
                 />
 
                 <Stack.Screen
                     name="ProductPage"
                     component={ProductPage}
 
-                    options={{
+                    options={({ navigation }) => ({
                         headerShown: true,
                         title: false,
                         headerBackTitleVisible: false,
@@ -188,11 +181,11 @@ const Navigator = () => {
 
                         headerTintColor: "#520000",
                         headerRight: () => (
-                            <TouchableOpacity>
+                            <TouchableOpacity onPress={() => navigation.navigate('MainTabs', { screen: 'CART' })}>
                                 <Image style={styles.basket} source={require('../images/shopping-bag.png')} />
                             </TouchableOpacity>
                         ),
-                    }}
+                    })}
                 />
 
             </Stack.Navigator>
@@ -203,8 +196,9 @@ export default Navigator
 
 const styles = StyleSheet.create({
     basket: {
-        width: 30,
-        height: 30
+        width: 25,
+        height: 25,
+        tintColor: '#520000',
     },
     heart: {
         width: 20,
