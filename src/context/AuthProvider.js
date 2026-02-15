f1e22203a64eeadbd19ccad7a899c5506d6dbc98
fatihsenguun <fatihsenguun@gmail.com>
2026-02-15 16:58:12 +0300

    favorite system

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useContext, useState } from "react"
 
 
 export const AuthContext = createContext()
@@ -18,3 +18,4 @@ export const AuthProvider = ({ children }) => {
 
     )
 }
+export const useAuth = useContext(AuthContext);
--- src/context/FavoriteContext.js
+++ src/context/FavoriteContext.js
@@ -1,41 +1,83 @@
 import { createContext, useEffect, useState } from "react";
-import {AuthProvider} from '../context/AuthProvider'
+import { useAuth } from './AuthProvider'
+import axios from "axios";
 const FavoritesContext = createContext();
 
-export const FavoritesProvider = ({children})=>{
+export const FavoritesProvider = ({ children }) => {
 
-    const {user}  = AuthProvider();
+    const { user } = useAuth();
     const [favorites, setFavorites] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-        
-    },[])
-    const fetchFavoritesFromBackend =()=>{
+
+    const BASE_URL = 'http://localhost:8080/rest/api/favorite'
+    useEffect(() => {
+
+        if (user) {
+            fetchFavoritesFromBackend();
+        } else {
+            setFavorites([]);
+        }
+    }, [user])
+
+    const fetchFavoritesFromBackend = async () => {
+        try {
+            const url = `${BASE_URL}/myfavorites`;
+
+            setLoading(true);
+            const response = await axios.get(url, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+
+            if (response.data) {
+
+                setFavorites(response.data.data);
+                console.log(response.data);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
 
     }
 
-    const toggleFavorite = (product)=>{
-        const isExist = favorites.find(item=>item.id === product.id)
+    const toggleFavorite = async (product) => {
+        const isExist = favorites.some(item => item.id === product.id)
 
+        const oldFavorites = [...favorites];
 
         if (isExist) {
-        setFavorites(favorites.filter(item=>item.id != product.id));
-            
+            setFavorites(favorites.filter(item => item.id != product.id));
+
+
         } else {
-            setFavorites([...favorites,product]);
+            setFavorites([...favorites, { product: product }]);
+        }
+        try {
+
+            const response = await axios.post(`${BASE_URL}/toggle`, {
+                product: product
+            })
+            console.log(response);
+
+        } catch (error) {
+
         }
 
 
+
+
+
     }
-    const isFavorite = (productId)=>{
-        return favorites.some(item=>item.id===productId)
+    const isFavorite = (productId) => {
+        return favorites.some(item => item.id === productId)
     };
 
-return(
-    <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite, loading }}>
+    return (
+        <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite, loading }}>
             {children}
         </FavoritesContext.Provider>
-)
+    )
 
 }
